edf00b93d84fccfb7431cf85032a3b63855aa0d4
Jordan Bishop <bishopj1292@gmail.com>
2017-06-18 15:44:01 -0500

    added basic routing functions

--- server/index.js
+++ server/index.js
@@ -4,13 +4,27 @@ var imdb = require('imdb-api');
 var config = require('../db/config');
 var movies = require('../db/schema/movie');
 var accounts = require('../db/schema/account');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 // console.log('hello', users, movies);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.render('/signin');
+});
+
+app.get('/signin', function (req, res) {
+  res.render('/signin');
+});
+
+app.get('/signup', function (req, res) {
+  res.render('/signup');
+});
+
+app.get('/index', function (req, res) {
+  res.render('/signup');
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
