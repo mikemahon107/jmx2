11eb2f16aa236887311a2c23d5d16e38a76678b1
Minh Nguyen <kuroemon95@gmail.com>
2017-06-19 11:04:27 -0500

    server-fixes, not completed

--- server/index.js
+++ server/index.js
@@ -9,12 +9,8 @@ var bcrypt = require('bcrypt-nodejs');
 
 app.use(express.static(__dirname.slice(0, __dirname.length - 6)));
 
-// console.log('hello', users, movies);
-
 app.get('/', function (req, res) {
-  // console.log(__dirname.slice(0, __dirname.length - 6));
   res.sendFile(__dirname.slice(0, __dirname.length - 6) + 'index.html')
-  // res.send('/index.html')
 });
 
 app.get('/signin', function (req, res) {
@@ -27,22 +23,21 @@ app.post('/signin', function (req, res) {
 
   accounts.findOne({username: username}, (err, user) => {
     if (!user) {
-      res.send('username does not exist.');
+      res.send('Incorrect password or username.');
       res.redirect('/login');
     } else {
       accounts.comparePassword(password, user.password, function(err, match) {
         if (match) {
           //create session
         } else {
+          res.send('Incorrect password or username.');
           res.redirect('/login');
         }
       });
-        //
-        res.redirect('/index');
-      });
+      res.redirect('/')
     }
-  });
-});
+  })
+})
 
 app.get('/signup', function (req, res) {
   res.render('/signup');
@@ -50,32 +45,24 @@ app.get('/signup', function (req, res) {
 
 app.post('/signup', function (req, res) {
   var username = req.body.username;
+  console.log(req.body)
   var password = req.body.password;
 
   accounts.findOne({username: username}, (err, user) => {
+    if (err) throw err
     if (user) {
       res.send('Username already exists');
       res.redirect('/signup');
     } else {
       accounts.insertOne({username: username, password: password}, () => {
         res.send('Account created.');
-        res.redirect('/index');
+        res.redirect('/');
       });
     }
   });
-  //find the username
-  //if result exists, redirect to signup w/ note that their username is taken
-  // else call accounts.insertOne w/ object of the username and pw
-
 })
 
-app.get('/index', function (req, res) {
-  res.render('/signup');
-});
 
-app.get('/movie', function (req, res) {
-  res.render('/movie');
-});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
