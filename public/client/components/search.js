78f584c40edfe84003f1dcf1fd5f3258d9020dcb
Misha Hoo <hoomisha@gmail.com>
2017-06-20 12:13:49 -0500

    implement search box handleClick

--- public/client/components/search.js
+++ public/client/components/search.js
@@ -2,14 +2,32 @@ angular.module('main-app') // copied mostly from ng-cast
 
 // .controller('SearchCtrl', function() {
 // })
+
+.controller('SearchController', function() {
+  this.handleClick = () => {
+    console.log(this.input);
+  };
+})
 .directive('search', function() {
   return {
+
     scope: {},
     restrict: 'E',
-    // controller: 'SearchCtrl',
-    // controllerAs: 'ctrl',
-    // bindToController: true,
+    controller: 'SearchController',
+    controllerAs: 'ctrl',
+    bindToController: true,
     templateUrl: 'public/client/templates/search.html'
-  };
+      };
 });
+
+// .directive('search', function() {
+//   return {
+//     scope: {},
+//     restrict: 'E',
+//     // controller: 'SearchCtrl',
+//     // controllerAs: 'ctrl',
+//     // bindToController: true,
+//     templateUrl: 'public/client/templates/search.html'
+//   };
+// });
 
