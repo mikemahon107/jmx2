270f07120c511a301a917fce400f8ef68b4f2036
Misha Hoo <hoomisha@gmail.com>
2017-06-20 13:39:41 -0500

    query OMBD on find movie button

--- public/client/components/search.js
+++ public/client/components/search.js
@@ -4,14 +4,23 @@ angular.module('main-app') // copied mostly from ng-cast
 // })
 
 .controller('SearchController', function() {
+  console.log('out', this.searchResult);
   this.handleClick = () => {
-    console.log(this.input);
-  };
+    this.service.search({t: this.input}, (data) => {
+      // this.searchResult.push(data)
+      console.log(data);
+    })
+  }
 })
+    // this.service.search(this.input, (data) => {
+    //   console.log('we are in', data);
 .directive('search', function() {
   return {
 
-    scope: {},
+    scope: {
+      service: '<',
+      searchResult: '<'
+    },
     restrict: 'E',
     controller: 'SearchController',
     controllerAs: 'ctrl',
