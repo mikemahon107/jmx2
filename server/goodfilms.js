b45d861d17cb4853558044c7dc4414b8711b4dce
Minh Nguyen <kuroemon95@gmail.com>
2017-06-19 21:32:42 -0500

    Clean up server files at the end of Monday

--- server/goodfilms.js
+++ server/goodfilms.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var imdb = require('imdb-api');
 var config = require('../db/config');
 var movies = require('../db/schema/movie');
 var accounts = require('../db/schema/account');
@@ -9,6 +8,7 @@ var bcrypt = require('bcrypt-nodejs');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var utility = require('./utility.js');
+var cookieParser = require('cookie-parser')
 
 app.use(express.static(__dirname.slice(0, __dirname.length - 6)));
 app.use(bodyParser.json());
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/signin', function (req, res) {
-  res.render('/signin');
+  //res.render('/signin');
 });
 
 app.post('/signin', function (req, res) {
@@ -33,7 +33,7 @@ app.post('/signin', function (req, res) {
   var password = req.body.password;
 
   accounts.findOne(username, (err, user) => {
-    if(err) throw err;
+    if (err) throw err;
     if (!user) {
       res.send('Incorrect password or username.');
       // res.redirect('/login');
@@ -42,7 +42,7 @@ app.post('/signin', function (req, res) {
         if (match) {
           //create session
           utility.createSession(req, res, user);
-          res.send('Everything works');
+          console.log('Everything works');
         } else {
           res.send('Incorrect password or username.');
           // res.redirect('/login');
@@ -53,17 +53,16 @@ app.post('/signin', function (req, res) {
 })
 
 app.get('/signup', function (req, res) {
-  res.render('/signup');
+  //res.render('signup');
 });
 
 app.post('/signup', function (req, res) {
-  console.log("REQ BODY here!", req.body)
   var username = req.body.username;
   var password = req.body.password;
 
   accounts.findOne(username, (err, user) => {
     if (err) throw err;
-    if (user.length) {
+    if (user) {
       console.log(user)
       res.send('Username already exists!');
       // res.redirect('/signup');
