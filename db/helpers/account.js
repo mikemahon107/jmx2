b67145cae12c6954f7c5be19e18afe9b870e0838
Jeremy Odell <jeremy0dell@yahoo.com>
2017-06-21 12:53:05 -0500

    refactor

---
+++ db/helpers/account.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+var bcrypt = require('bcrypt-nodejs');
+var Account = require('../schema/account.js');
+
+console.log(Account);
+
+function comparePassword(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    if (err) { return cb(err); }
+    cb(null, isMatch);
+  });
+};
+
+function findAll(cb) {
+  Account.find({}, cb);
+}
+
+function findOne(username, cb) {
+  Account.findOne({username: username}, cb);
+}
+
+function insertOne(user, cb) {
+  bcrypt.hash(user.password, null, null, function(a,b,c) {
+    console.log('a: ', a ,'b: ', b, 'c: ', c); // this just changes the password at insertion
+    user.password = b;
+    Account.create(user, cb);
+  });
+};
+
+exports.comparePassword = comparePassword;
+exports.findOne = findOne;
+exports.findAll = findAll;
+exports.insertOne = insertOne;
