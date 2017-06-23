e90fe3b20be6bc0964aedffe3c2299b7778fcf84
Jeremy Odell <jeremy0dell@yahoo.com>
2017-06-23 18:43:54 -0500

    fixing /user redirect and other problems

--- public/client/index.js
+++ public/client/index.js
@@ -19,4 +19,30 @@ angular.module('main-app', ['ngRoute'])
         .otherwise({ redirectTo: '/' });
 
     $locationProvider.html5Mode(true);
-});
+})
+.run(function($rootScope, $location, $http) {
+  console.log('hello guys!!!', $rootScope);
+        // register listener to watch route changes
+    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+      console.log('this is being triggered!', $location.$$path);
+      $http.get('/userCheck').then( (session) => {
+        console.log('true?', $location.$$path === '/user' && !session.user);
+        console.log('session is ', session);
+        if ($location.$$path === '/user' && !session.data.user) {
+          // session.user = {};
+          $location.path( "/login" );
+        } else if (session.data.user) {
+          $location.path( "/user" );
+        }
+      });
+      // if ( $rootScope.loggedUser == null ) {
+      //   // no logged user, we should be going to #login
+      //   if ( next.templateUrl == "partials/login.html" ) {
+      //     // already going to #login, no redirect needed
+      //   } else {
+      //     // not going to #login, we should redirect now
+      //     $location.path( "/login" );
+      //   }
+      // }         
+    });
+ })
