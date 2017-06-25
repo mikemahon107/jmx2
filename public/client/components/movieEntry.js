b29f1c4cbf6bf9fa5492ded735ff86ad4b3f10af
Jeremy Odell <jeremy0dell@yahoo.com>
2017-06-25 17:55:15 -0500

    mostly fix refreshing

--- public/client/components/movieEntry.js
+++ public/client/components/movieEntry.js
@@ -15,13 +15,16 @@ angular.module('main-app') // copied mostly from ng-cast
           this.movie.details.Poster === "N/A" || !this.movie.details.Poster ? this.movie.details.Poster = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png' : this.movie.details.Poster
         })
       };
+      
+
       this.handleAddToFavorites = function() {
         var idList = this.user.watched.map((x) => x.imdb_id);
+        var i = idList.indexOf(this.movie.imdb_id);
 
         $http.post('/addFavorite', {user: this.user.username,
         movie: this.user.watched[idList.indexOf(this.movie.imdb_id)]}).then(() => {
           $http.get('/sess').then((session) => {
-            this.user.watched = session.data.watched;
+            this.user.watched[i].isFavorite = session.data.watched[i].isFavorite;
           });
         });
 
@@ -34,12 +37,14 @@ angular.module('main-app') // copied mostly from ng-cast
         // console.log('movie title', this.movie.details.Title);
         // console.log('movie year', this.movie.details.Year);
         // console.log('input', this.input);
+        var idList = this.user.watched.map((x) => x.imdb_id);
+        var i = idList.indexOf(this.movie.imdb_id);
 
         // $http.post('/addComment', {user: this.user, movieTitle: this.movie.title});
         $http.post('/addComment', {user: this.user.username, imdb_id: this.movie.imdb_id, comment: this.input}).then(() => {
           console.log('trying to add a comment!')
           $http.get('/sess').then((session) => {
-            this.user.watched = session.data.watched;
+            this.user.watched[i].comment = session.data.watched[i].comment;
             // console.log('Movie is: ', this.movie, 'User is: ', this.user);
             // //fruits.indexOf("Apple")
             // console.log('the ids in movies are: ', this.user.watched.map((x) => x.imdb_id))
@@ -49,27 +54,29 @@ angular.module('main-app') // copied mostly from ng-cast
       };
 
       this.handleRatingClick = function(rating) {
-          console.log('username', this.user.username);
-          console.log('this', this);
-          console.log('movie title', this.movie.details.Title);
-          console.log('movie year', this.movie.details.Year);
-          console.log('input this is a log', this.rating);
-          console.log('THE TRUE RATING IS ', rating)
+        console.log('username', this.user.username);
+        console.log('this', this);
+        console.log('movie title', this.movie.details.Title);
+        console.log('movie year', this.movie.details.Year);
+        console.log('input this is a log', this.rating);
+        console.log('THE TRUE RATING IS ', rating)
+        var idList = this.user.watched.map((x) => x.imdb_id);
+        var i = idList.indexOf(this.movie.imdb_id);
 
-          $http.post('/editRating', {user: this.user.username, imdb_id: this.movie.imdb_id, rating: this.rating}).then(() => {
-            $http.get('/sess').then((session) => {
-              this.user.watched = session.data.watched;
-            });
+        $http.post('/editRating', {user: this.user.username, imdb_id: this.movie.imdb_id, rating: this.rating}).then(() => {
+          $http.get('/sess').then((session) => {
+            this.user.watched[i].rating = session.data.watched[i].rating;
           });
-        };
+        });
+      };
 
-        this.handleRemoveClick = function() {
-          $http.post('/removeFromWatched', {user: this.user.username, imdb_id: this.movie.imdb_id}).then(() => {
-            $http.get('/sess').then((session) => {
-              this.user.watched = session.data.watched;
-            });
+      this.handleRemoveClick = function() {
+        $http.post('/removeFromWatched', {user: this.user.username, imdb_id: this.movie.imdb_id}).then(() => {
+          $http.get('/sess').then((session) => {
+            this.user.watched = session.data.watched;
           });
-        };
+        });
+      };
 
 
 
