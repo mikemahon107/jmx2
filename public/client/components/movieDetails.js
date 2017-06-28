b8938e7a7cea2c2d2193a6665503a61bcee937c6
Nate Green <nate.exe@gmail.com>
2017-06-27 19:00:40 -0500

    Add movie view

---
+++ public/client/components/movieDetails.js
@@ -0,0 +1,15 @@
+angular.module('main-app')
+
+.directive('movieDetails', function() {
+  return {
+    scope: {
+
+    },
+    controller: function() {
+
+    },
+    controllerAs: 'ctrl',
+    bindToController: true,
+    templateUrl: 'public/client/templates/movieDetails.html'
+  };
+});
