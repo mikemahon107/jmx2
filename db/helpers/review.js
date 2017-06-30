fdd5e5b82f5bf2082323be25f1e65eab9f9d7e22
Ada323 <mag242@gmail.com>
2017-06-30 12:30:16 -0500

    Mostly implement upvotes/downvotes, still a minor bug in removeDownVote

--- db/helpers/review.js
+++ db/helpers/review.js
@@ -8,9 +8,9 @@ function findAll(imdb_id, cb) {
   Review.find({'imdb_id': imdb_id}, cb);
 };
 
-function findOne(imdb_id, cb) {
-  console.log('imdb_id', imdb_id)
-  Review.findOne({imdb_id: imdb_id}, cb);
+function findOne(params, cb) {
+  // params should be an object structured like this: {user: user, date: date}
+  Review.findOne(params, cb);
 };
 
 function insertReview(review) {
@@ -26,35 +26,111 @@ function insertReview(review) {
   })
 };
 
-function insertUserIntoUpvote(user, date) {
+function insertUserIntoUpvote(imdb_id, user, date, clickUser) {
   // search findOne with {user: USER, date: DATE}
   // push user into upvote array
+  Review.findOne({imdb_id: imdb_id, user: user, date: date}, function(err, review) {
+    if (err) {
+      console.log('error in upvote', err);
+    } else {
+      if (!review.upvotes.includes(clickUser)) {
+        review.upvotes.push(clickUser)
+        review.save();
+        console.log('upvote saved!')
+      }
+    }
+  })
 }
 
-function removeUserFromUpvote(user, date) {
+function removeUserFromUpvote(imdb_id, user, date, clickUser) {
   // search findOne with {user: USER, date: DATE}
   //splice user out of upvote array
+  Review.findOne({imdb_id: imdb_id, user: user, date: date}, function(err, review) {
+    if (err) {
+      console.log('error in upvote', err);
+    } else {
+      for (var i = 0; i < review.upvotes.length; i++) {
+        if (review.upvotes[i] === clickUser) {
+            review.upvotes.splice(i, 1);
+            console.log('upvote removed')
+            break;
+        }
+      }
+      review.save();
+    }
+  })
 }
 
-function insertUserIntoDownvote(user, date) {
+function insertUserIntoDownvote(imdb_id, user, date, clickUser) {
   // search findOne with {user: USER, date: DATE}
   //push user into downvote array
+  console.log('CLICKUSER', clickUser);
+  Review.findOne({imdb_id: imdb_id, user: user, date: date}, function(err, review) {
+    if (err) {
+      console.log('error in upvote', err);
+    } else {
+      if (!review.downvotes.includes(clickUser)) {
+        review.downvotes.push(clickUser)
+        review.save();
+        console.log('downvotes saved!')
+      }
+    }
+  })
 }
 
-function removeUserFromDownvote(user, date){
+function removeUserFromDownvote(imdb_id, user, date, clickUser){
   // search findOne with {user: USER, date: DATE}
   //splice user out of upvote array
+  console.log('in REMOVE USER FROM DOWNVOTE')
+  Review.findOne({imdb_id: imdb_id, user: user, date: date}, function(err, review) {
+    if (err) {
+      console.log('error in upvote', err);
+    } else {
+      for (var i = 0; i < review.downvotes.length; i++) {
+        if (review.downvotes[i] === clickUser) {
+            review.downvotes.splice(i, 1);
+            console.log('downvote removed')
+          break;
+        }
+      }
+      review.save();
+    }
+  })
 }
 
-function incrementScore() {
+function incrementScore(imdb_id, user, date) {
   // add one to score
+  Review.findOne({imdb_id: imdb_id, user: user, date: date}, function(err, review) {
+    if (err) {
+      console.log('error in incrementScore', err);
+    } else {
+      review.score += 1;
+      review.save();
+      console.log('score incremented!');
+    }
+  })
 }
 
-function decrementScore() {
+function decrementScore(imdb_id, user, date) {
   // subtract one from score
+  Review.findOne({imdb_id: imdb_id, user: user, date: date}, function(err, review) {
+    if (err) {
+      console.log('error in decrementScore', err);
+    } else {
+      review.score -= 1;
+      review.save();
+      console.log('score decremented!');
+    }
+  })
 }
 
 
 exports.findAll = findAll;
 exports.findOne = findOne;
 exports.insertReview = insertReview;
+exports.insertUserIntoUpvote = insertUserIntoUpvote;
+exports.removeUserFromUpvote = removeUserFromUpvote;
+exports.insertUserIntoDownvote = insertUserIntoDownvote;
+exports.removeUserFromDownvote = removeUserFromDownvote;
+exports.incrementScore = incrementScore;
+exports.decrementScore = decrementScore;
